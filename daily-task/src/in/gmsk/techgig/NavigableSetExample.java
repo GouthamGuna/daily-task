1cbf142fdc1be719cb9048b4355b668d5556cef7
Goutham <ggowthamsankar@gmail.com>
2023-08-05 21:56:59 +0530

    Update NavigableSetExample.java

--- daily-task/src/in/gmsk/techgig/NavigableSetExample.java
+++ daily-task/src/in/gmsk/techgig/NavigableSetExample.java
@@ -1,4 +1,18 @@
 package in.gmsk.techgig;
 
-public class NavigableSet {
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
+public class NavigableSetExample {
+
+    public static void main(String[] args) {
+
+        NavigableSet<Integer> setNumber = new TreeSet<>();
+        setNumber.add(11);
+        setNumber.add(12);
+        setNumber.add(13);
+        setNumber.headSet(14);
+
+        System.out.println(setNumber);
+    }
 }
