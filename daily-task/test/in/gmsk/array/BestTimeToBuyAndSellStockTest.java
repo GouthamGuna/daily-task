6efca44bbc7462b16bc031aabea846f0afe406a8
Goutham <ggowthamsankar@gmail.com>
2023-08-06 07:29:44 +0530

    Update BestTimeToBuyAndSellStockTest.java

--- daily-task/test/in/gmsk/array/BestTimeToBuyAndSellStockTest.java
+++ daily-task/test/in/gmsk/array/BestTimeToBuyAndSellStockTest.java
@@ -1,9 +1,14 @@
 package in.gmsk.array;
 
-import static org.junit.jupiter.api.Assertions.*;
-class BestTimeToBuyAndSellStockTest {
+import org.junit.jupiter.api.Test;
 
-    @org.junit.jupiter.api.Test
-    void maxProfit() {
+public class BestTimeToBuyAndSellStockTest {
+
+    @Test
+    public void maxProfit() {
+
+        BestTimeToBuyAndSellStock instance = new BestTimeToBuyAndSellStock();
+        int[] arrayOfAmount = {11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        System.out.println(instance.maxProfit(arrayOfAmount));
     }
 }
