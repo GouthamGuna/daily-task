7e4fe47e547428c6065c9e4aed7b7581b719951c
Goutham <ggowthamsankar@gmail.com>
2023-08-06 07:25:23 +0530

    initial commit

---
+++ daily-task/src/in/gmsk/array/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,4 @@
+package in.gmsk.array;
+
+public class BestTimeToBuyAndSellStock {
+}
---
+++ daily-task/test/in/gmsk/array/BestTimeToBuyAndSellStockTest.java
@@ -0,0 +1,9 @@
+package in.gmsk.array;
+
+import static org.junit.jupiter.api.Assertions.*;
+class BestTimeToBuyAndSellStockTest {
+
+    @org.junit.jupiter.api.Test
+    void maxProfit() {
+    }
+}
