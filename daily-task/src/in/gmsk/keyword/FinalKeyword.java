58e7eaced57dc6ac31660251b1e1842b7f99e9dd
Goutham <ggowthamsankar@gmail.com>
2023-08-07 23:04:49 +0530

    Update FinalKeyword.java

--- daily-task/src/in/gmsk/keyword/FinalKeyword.java
+++ daily-task/src/in/gmsk/keyword/FinalKeyword.java
@@ -1,2 +1,41 @@
-package in.gmsk.keyword;public class FinalKeyword {
+package in.gmsk.keyword;
+
+class Example{
+
+    int speedLimit = 40; //instance variable
+
+    final int speedLimitFinal = 200; //final instance variable
+
+    final int blankSpeedLimit; //final blank instance variable
+
+    /*Example(int blankSpeedLimit) {
+        this.blankSpeedLimit = blankSpeedLimit;
+    }*/
+
+   {
+        blankSpeedLimit = 180;
+    }
+
+    public void exampleMethodOne() {
+        // speedLimitFinal = 250;
+        speedLimit = 60;
+    }
+
+    final void exampleMethodTwo(){
+        final int speed =  20;
+    }
+
+    void exampleMethodThree(final int speed){
+        System.out.println("speed : "+speed);
+    }
+}
+
+public class FinalKeyword extends Example{
+
+    public static void main(String[] args) {
+
+        FinalKeyword instance = new FinalKeyword();
+        // speedLimit = 100;
+        instance.exampleMethodThree(instance.speedLimitFinal);
+    }
 }
