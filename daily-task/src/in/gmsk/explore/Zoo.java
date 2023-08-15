4f7925a91668c3170371daa267924a9cc4e4a8db
Goutham <ggowthamsankar@gmail.com>
2023-08-15 18:05:24 +0530

    Update Zoo.java

--- daily-task/src/in/gmsk/explore/Zoo.java
+++ daily-task/src/in/gmsk/explore/Zoo.java
@@ -1,2 +1,23 @@
-package in.gmsk.explore;public class Zoo {
+package in.gmsk.explore;
+
+public class Zoo {
+    String name;
+
+    public Zoo(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+
+        var name = new Zoo("Java Zoo").getName();
+        System.out.println(name);
+    }
 }
