8e59b7d26d8d5ea26eb7bb569ce7bae9ff7f7a4d
Goutham <ggowthamsankar@gmail.com>
2023-08-16 08:48:45 +0530

    Update FinalLocalVariables.java

--- ocp_1Z0-819/src/fundamentals/FinalLocalVariables.java
+++ ocp_1Z0-819/src/fundamentals/FinalLocalVariables.java
@@ -1,2 +1,19 @@
-package fundamentals;public class FinalLocalVariables {
+package fundamentals;
+
+public class FinalLocalVariables {
+
+    public static void printZooInfo(boolean isWeekend){
+
+        final int giraffe = 5;
+        final long lemur;
+
+        if(isWeekend) lemur = 5;
+        else lemur = 10;
+
+        System.out.println(giraffe +" "+ lemur);
+    }
+
+    public static void main(String[] args) {
+        printZooInfo(false);
+    }
 }
