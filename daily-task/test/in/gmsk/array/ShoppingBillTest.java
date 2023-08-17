1380f431c1c1d62407ce2694ff431a42b1734235
Goutham <ggowthamsankar@gmail.com>
2023-08-17 08:36:18 +0530

    Update ShoppingBillTest.java

--- daily-task/test/in/gmsk/array/ShoppingBillTest.java
+++ daily-task/test/in/gmsk/array/ShoppingBillTest.java
@@ -1,6 +1,13 @@
 package in.gmsk.array;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ShoppingBillTest {
-  
+    @Test
+    void displayBillTest(){
+        ShoppingBill instance = new ShoppingBill();
+        instance.displayBill();
+    }
 }
