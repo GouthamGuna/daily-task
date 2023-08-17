cfa37aded1d9d600662d3aa651239825b3e7a455
Goutham <ggowthamsankar@gmail.com>
2023-08-17 08:43:18 +0530

    Update MainMethodArgs.java

--- daily-task/src/in/gmsk/array/MainMethodArgs.java
+++ daily-task/src/in/gmsk/array/MainMethodArgs.java
@@ -1,4 +1,16 @@
 package in.gmsk.array;
 
 public class MainMethodArgs {
+    public static void main(String[] args) {
+        boolean help = false;
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("-h")) {
+                help = true;
+            }
+        }
+        if (help) {
+            System.out.println("Usage: java Main [-h]");
+            System.exit(0);
+        }
+    }
 }
