8e56b363eed753339ae1d8270eb314d145f262ae
Goutham <ggowthamsankar@gmail.com>
2023-08-18 22:38:18 +0530

    Update PolarBear.java

--- ocp_1Z0-819/src/fundamentals/final_modifier/PolarBear.java
+++ ocp_1Z0-819/src/fundamentals/final_modifier/PolarBear.java
@@ -1,2 +1,18 @@
-package fundamentals.final_modifier;public class PolarBear {
+package fundamentals.final_modifier;
+
+public class PolarBear {
+
+    final int age = 10;
+    final int fishEaten;
+    final String name;
+
+    {fishEaten = 20;} // instance initializer
+
+    public PolarBear(){
+        name = "lemur";
+    }
+    
+    public PolarBear(int height){
+        this();
+    }
 }
