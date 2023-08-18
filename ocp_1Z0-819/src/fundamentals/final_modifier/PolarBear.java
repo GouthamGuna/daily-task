8d2aafc8bf245267fba8d5207f0ab2aac971faaf
Goutham <ggowthamsankar@gmail.com>
2023-08-18 23:43:11 +0530

    Update PolarBear.java

--- ocp_1Z0-819/src/fundamentals/final_modifier/PolarBear.java
+++ ocp_1Z0-819/src/fundamentals/final_modifier/PolarBear.java
@@ -2,7 +2,21 @@ package fundamentals.final_modifier;
 
 public class PolarBear {
 
-    final int age = 10;
+    /**
+     *  The rule for static final variables are similar to instance final variables, except they
+     *  do not use static constructor.
+     * <p>
+     *  public class PolarBear{
+     *      final static String name = "Sree";
+     *      static final int bamboo;
+     *      static final double height; // DOES NOT COMPILE
+     *      static { bamboo = 5;}
+     *  }
+     * <p>
+     *   The height variable is not assigned a value anywhere in the class definition.
+     * */
+
+     final int age = 10;
     final int fishEaten;
     final String name;
 
@@ -14,5 +28,13 @@ public class PolarBear {
     
     public PolarBear(int height){
         this();
+    }
+
+    public static void main(String[] args) {
+        PolarBear instance = new PolarBear();
+
+        System.out.println(instance.age);
+        System.out.println(instance.fishEaten);
+        System.out.println(instance.name);
     }
 }
