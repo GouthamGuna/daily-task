e034e16aa233dc0e7686bb57e077ec7bfa2f0f4c
Goutham <ggowthamsankar@gmail.com>
2023-08-20 18:51:59 +0530

    Update Hippo.java

--- ocp_1Z0-819/src/fundamentals/final_modifier/method/Hippo.java
+++ ocp_1Z0-819/src/fundamentals/final_modifier/method/Hippo.java
@@ -1,2 +1,20 @@
-package fundamentals.final_modifier.method;public class Hippo {
+package fundamentals.final_modifier.method;
+
+public class Hippo extends Animal{
+
+    /**
+     *  Methods marked final cannot be overridden by a subclass.
+     *  This essentially prevents any polymorphic behavior on the method call and a specific
+     *  version of the method is always called.
+     *<p>
+     *  Remember that method can be assigned an abstraction or final modifier. An abstraction method is one
+     *  that does not define a method body and can appear only onside an abstract class or interface.
+     *<p>
+     *  A final method is one that cannot overridden by a subclass.
+     */
+
+    @Override
+     final void chew() {    // using final keyword.
+        System.out.println("Using final keyword in chew method.");
+    }
 }
