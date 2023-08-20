e478b9509ac76e24ec27c1f8c09d284061d601fa
Goutham <ggowthamsankar@gmail.com>
2023-08-20 18:52:33 +0530

    Update PygmyHippo.java

--- ocp_1Z0-819/src/fundamentals/final_modifier/method/PygmyHippo.java
+++ ocp_1Z0-819/src/fundamentals/final_modifier/method/PygmyHippo.java
@@ -1,2 +1,7 @@
-package fundamentals.final_modifier.method;public class PygmyHippo {
+package fundamentals.final_modifier.method;
+
+public class PygmyHippo extends Hippo{
+
+    // void chew(){} // DOES NOT COMPILE
+
 }
