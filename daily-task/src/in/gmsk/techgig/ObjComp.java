acc07f1058779e6d43a7d71a22a7ef885ec2ad82
Goutham <ggowthamsankar@gmail.com>
2023-08-30 07:37:09 +0530

    Update ObjComp.java

--- daily-task/src/in/gmsk/techgig/ObjComp.java
+++ daily-task/src/in/gmsk/techgig/ObjComp.java
@@ -1,4 +1,27 @@
 package in.gmsk.techgig;
 
-public class Objcomp {
+public class ObjComp {
+    public static void main(String[] args) {
+        int result = 0;
+        ObjComp objComp = new ObjComp();
+        Object o = objComp;
+
+        if(o == objComp){
+            result = 1;
+        }
+
+        if(o != objComp){
+            result = result + 1;
+        }
+
+        if(o.equals(objComp)){
+            result = result + 100;
+        }
+
+        if(objComp.equals(o)){
+            result = result + 1000;
+        }
+
+        System.out.println("result = " + result);
+    }
 }
