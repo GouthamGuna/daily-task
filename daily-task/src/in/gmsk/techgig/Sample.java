a1cdaf0ac8564bdd3020e1da74f5bac4a936a365
Goutham <ggowthamsankar@gmail.com>
2023-09-02 11:01:55 +0530

    Update Sample.java

--- daily-task/src/in/gmsk/techgig/Sample.java
+++ daily-task/src/in/gmsk/techgig/Sample.java
@@ -1,4 +1,17 @@
 package in.gmsk.techgig;
 
 public class Sample {
+
+    public static void main(String[] args) {
+
+        Sample obj = new Sample();
+
+        for(int a=0; a < 5; ++a){
+
+           // obj.set(a); // Compile time err
+            // obj.clear(2); // Compile time err
+
+            System.out.println(obj);
+        }
+    }
 }
