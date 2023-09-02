15fb4a56f8867e73cad766e9028c7191a9a20dc1
Goutham <ggowthamsankar@gmail.com>
2023-09-02 11:03:17 +0530

    Update ArrayExampleTest.java

--- daily-task/test/in/gmsk/techgig/ArrayExampleTest.java
+++ daily-task/test/in/gmsk/techgig/ArrayExampleTest.java
@@ -1,5 +1,13 @@
-import static org.junit.jupiter.api.Assertions.*;
+package in.gmsk.techgig;
+
+import org.junit.jupiter.api.Test;
 
 class ArrayExampleTest {
 
+    @Test
+    void arrayCodeFragment() {
+        String values = "Word1 Word2 Word3 Word5";
+        ArrayExample obj = new ArrayExample();
+        obj.arrayCodeFragment(values);
+    }
 }
