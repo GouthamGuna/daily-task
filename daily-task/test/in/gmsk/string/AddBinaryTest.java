7282dfc33f8709ed1880ab269c96d7a90d6cad04
Goutham <ggowthamsankar@gmail.com>
2023-09-02 11:02:55 +0530

    Update AddBinaryTest.java

--- daily-task/test/in/gmsk/string/AddBinaryTest.java
+++ daily-task/test/in/gmsk/string/AddBinaryTest.java
@@ -4,11 +4,11 @@ import org.junit.jupiter.api.Test;
 
 class AddBinaryTest {
     @Test
-    void addBinary(){
+    void addBinary() {
 
         AddBinary instance = new AddBinary();
         String args1 = "11", args2 = "1";
-        System.out.println("Output : "+ instance.addBinary(args1, args2));
+        System.out.println("Output : " + instance.addBinary(args1, args2));
     }
 
 }
