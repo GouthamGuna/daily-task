eb6461ba3bd51f713d85d08eef96143c7ac27358
Goutham <ggowthamsankar@gmail.com>
2023-09-02 11:00:46 +0530

    Update ArrayExample.java

--- daily-task/src/in/gmsk/techgig/ArrayExample.java
+++ daily-task/src/in/gmsk/techgig/ArrayExample.java
@@ -1,4 +1,24 @@
 package in.gmsk.techgig;
 
+import java.util.Arrays;
+
+import static java.lang.System.*;
+
 public class ArrayExample {
+
+    public void arrayCodeFragment(String values) {
+
+        String[] val = values.split(" ");
+        Arrays.sort(val);
+
+        Arrays.stream(val).forEach(out::println);
+    }
+
+
+
+    public static void main(String[] args) {
+        String values = "Word1 Word2 Word3 Word5";
+        ArrayExample obj = new ArrayExample();
+        obj.arrayCodeFragment(values);
+    }
 }
