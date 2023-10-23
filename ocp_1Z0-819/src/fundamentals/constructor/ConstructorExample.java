1c94db2f7a6d0c9baef5491573238a4eb5256350
Goutham <ggowthamsankar@gmail.com>
2023-10-23 10:55:34 +0530

    remove .ideal

--- ocp_1Z0-819/src/fundamentals/constructor/ConstructorExample.java
+++ ocp_1Z0-819/src/fundamentals/constructor/ConstructorExample.java
@@ -1,2 +1,29 @@
-package fundamentals.constructor;public class ConstructorExample {
+package fundamentals.constructor;
+
+class Base{
+    int counter = 1000;
+
+    Base(){
+        Base();
+    }
+
+    void Base(){
+        System.out.println("Counter = "+(++counter));
+    }
+}
+
+class Derived extends Base{
+    int counter = 2000;
+
+    Derived(){}
+
+    void Base(){
+        System.out.println("Derived = "+(--counter));
+    }
+}
+
+public class ConstructorExample {
+    public static void main(String[] args) {
+        Base base = new Derived();
+    }
 }
--- ocp_1Z0-819/src/fundamentals/static_modifier/StaticExample.java
+++ ocp_1Z0-819/src/fundamentals/static_modifier/StaticExample.java
@@ -1,2 +1,21 @@
-package fundamentals.static_modifier;public class StaticExample {
+package fundamentals.static_modifier;
+
+public class StaticExample {
+
+    static {
+        getMethodA();
+       // iamNotStaticMethodA(); // compiler error cannot reference from static context.
+    }
+
+    static void getMethodA(){
+        System.out.println("I am Method A");
+    }
+    void iamNotStaticMethodA(){
+        System.out.println("I am Method A - Non static");
+    }
+
+    public static void main(String[] args) {
+        StaticExample example = new StaticExample();
+        example.iamNotStaticMethodA();
+    }
 }
