3897963667526178c34624c73c4f52ddd8998705
Goutham <ggowthamsankar@gmail.com>
2023-11-09 14:08:06 +0530

    Update ThreadA & B .java

--- ocp_1Z0-819/src/threading/ThreadA.java
+++ ocp_1Z0-819/src/threading/ThreadA.java
@@ -1,4 +1,19 @@
 package threading;
 
 public class ThreadA {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.err.println("Main method thread name = "+Thread.currentThread().getName());
+
+        ThreadB threadB = new ThreadB();
+        threadB.start();
+
+        // Thread.sleep(1000); // Bad Practices. Never put it in sleep method.
+
+        synchronized (threadB) {
+            threadB.wait(10000);   // Good Practices for when your using wait method put it timeout.
+            System.out.println("threadB = " + threadB.total);
+        }
+    }
 }
--- ocp_1Z0-819/src/threading/ThreadB.java
+++ ocp_1Z0-819/src/threading/ThreadB.java
@@ -1,4 +1,25 @@
 package threading;
 
-public class ThreadB {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThreadB extends Thread {
+
+    List<Integer> total = new ArrayList<>();
+
+    @Override
+    public void run() {
+
+        System.err.println("Run method thread name = " + Thread.currentThread().getName());
+
+        synchronized (this) {
+
+            int counter = 0;
+
+            for (int i = 0; i <= 100; i++) {
+                total.add(counter += i);
+            }
+            this.notify();
+        }
+    }
 }
