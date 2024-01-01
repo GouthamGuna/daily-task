dc1a140a6cfdc534835f51a22743e62ea82b589d
GouthamGuna <ggowthamsankar@gmail.com>
2024-01-01 21:48:22 +0530

    Update CommonClass.java

--- ocp_1Z0-819/src/fundamentals/arrays/CommonClass.java
+++ ocp_1Z0-819/src/fundamentals/arrays/CommonClass.java
@@ -4,8 +4,17 @@ public class CommonClass {
     public static void main(String[] args) {
 
         int[] numbers = new int[5];
+        numbers[0] = 0;
+        numbers[1] = 1;
+        numbers[2] = 2;
+        numbers[3] = 3;
+        numbers[4] = 4;
+        numbers[5] = 5;
+        numbers[6] = 6;
 
         int length = numbers.length;
+
+        System.out.println( "length = " + length );
 
         System.out.println("length = " + numbers[length]);
 
