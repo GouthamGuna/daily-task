b1913dca251b71492dd5238c9081a34992b11a8d
GouthamGuna <ggowthamsankar@gmail.com>
2024-01-24 22:06:23 +0530

    Update ConcurrentMain.java

--- learning_time_17/src/main/thread/ConcurrentMain.java
+++ learning_time_17/src/main/thread/ConcurrentMain.java
@@ -2,30 +2,37 @@ package main.thread;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 class Base {
 
-    static void getPlayWithConcurrentThread() {
+    static void getPlayWithConcurrentThread(int range) {
 
-        class InnerClassForConcurrentThreadMethod extends Thread {
+        System.out.println( "range = " + range );
+
+        class InnerClassForConcurrentThreadMethod implements Runnable {
 
             final int counter = 100;
+
+            @Override
+            public void run() {
+                printStudentList();
+            }
 
             void printStudentList() {
                 System.out.println( "printStudentList() Thread Name : " + Thread.currentThread().getName() );
                 for (int i = 0; i <= counter; i++) {
                     Stream<String> student = Base.getStudent();
 
-                    int finalI = i;
-                    student.forEach( x -> System.out.println( "count = " + (finalI) + " " + x ) );
+                    student.forEach( x -> System.out.println( "count = " + (range) + " " + x ) );
                 }
             }
         }
 
         InnerClassForConcurrentThreadMethod Obj = new InnerClassForConcurrentThreadMethod();
-        Obj.start();
-        Obj.printStudentList();
+        Thread thread = new Thread(Obj);
+        thread.start();
     }
 
     static Stream<String> getStudent() {
@@ -48,6 +55,6 @@ public class ConcurrentMain {
     public static void main(String[] args) {
         System.out.println( "main() Thread Name : " + Thread.currentThread().getName() );
 
-        Base.getPlayWithConcurrentThread();
+        IntStream.range( 1, 100 ).forEach( Base::getPlayWithConcurrentThread );
     }
 }
