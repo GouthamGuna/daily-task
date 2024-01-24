58ba36f3adc472ba49cbf9689416c668a30fe5a7
GouthamGuna <ggowthamsankar@gmail.com>
2024-01-24 20:38:20 +0530

    Update ExampleAbstraction.java

--- learning_time_17/src/main/general/ExampleAbstraction.java
+++ learning_time_17/src/main/general/ExampleAbstraction.java
@@ -1,2 +1,18 @@
-package main.general;public class ExampleAbstraction {
+package main.general;
+
+public abstract class ExampleAbstraction {
+
+    abstract void foo();
+
+    public static void main(String[] args) { // Is subclassed by Anonymous in main in ExampleAbstraction (main.general)
+
+        ExampleAbstraction obj = new ExampleAbstraction() {
+            @Override
+            void foo() {
+                System.out.println("foo!");
+            }
+        };
+
+        obj.foo();
+    }
 }
