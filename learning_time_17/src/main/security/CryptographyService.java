1a6cbaf552acd85e9d0d23da88b65c546835c501
GouthamGuna <ggowthamsankar@gmail.com>
2024-02-20 14:34:42 +0530

    Update CryptographyService.java

--- learning_time_17/src/main/security/CryptographyService.java
+++ learning_time_17/src/main/security/CryptographyService.java
@@ -1,2 +1,16 @@
-package main.security;public interface CryptographyService {
+package main.security;
+
+public interface CryptographyService {
+
+    void encodeStringUsingTheBasicBase64(String originalInput);
+
+    void decodeStringUsingTheBasicBase64(String encodedString);
+
+    void encodeURLAndFileNameBase64(String originalURI);
+
+    void decodeURLAndFileNameBase64(String encodedUrl);
+
+    void encodeStringUsingMIMETypeBase64(String originalInput);
+
+    void decodeStringUsingMIMETypeBase64(String encodedString);
 }
