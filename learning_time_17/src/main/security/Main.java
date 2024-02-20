b40febdaffee0985e5ce03b9b0351523f85e9e8d
GouthamGuna <ggowthamsankar@gmail.com>
2024-02-20 14:35:14 +0530

    Update Main.java

--- learning_time_17/src/main/security/Main.java
+++ learning_time_17/src/main/security/Main.java
@@ -1,4 +1,21 @@
 package main.security;
 
-public class LearnBase64 {
+public class Main {
+    private static final CryptographyService SERVICE = new CryptographyServiceImpl();
+
+    public static void main(String[] args) {
+
+
+        SERVICE.encodeStringUsingTheBasicBase64( "GMSK" ); // R01TSw==
+
+        SERVICE.decodeStringUsingTheBasicBase64( "R01TSw==" );
+
+        SERVICE.encodeURLAndFileNameBase64( "http://sms.cerpsoft.in/<App>/api.do?method=getLinkedMetaData" );
+
+        SERVICE.decodeURLAndFileNameBase64( "aHR0cDovL3Ntcy5jZXJwc29mdC5pbi9wZWFybC9hcGkuZG8_bWV0aG9kPWdldExpbmtlZE1ldGFEYXRh" );
+
+        SERVICE.encodeStringUsingMIMETypeBase64( "GMSK" ); // R01TSw==
+
+        SERVICE.decodeStringUsingMIMETypeBase64( "R01TSw==" );
+    }
 }
