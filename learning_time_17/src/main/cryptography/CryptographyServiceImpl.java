1c09f2d5fcf882f8db44d854fc6de960f3d8fafc
GouthamGuna <ggowthamsankar@gmail.com>
2024-02-21 19:43:28 +0530

    Update CryptographyServiceImpl.java

--- learning_time_17/src/main/cryptography/CryptographyServiceImpl.java
+++ learning_time_17/src/main/cryptography/CryptographyServiceImpl.java
@@ -1,6 +1,8 @@
-package main.security;
+package main.cryptography;
 
 import java.util.Base64;
+import java.util.SimpleTimeZone;
+import java.util.stream.Collectors;
 
 public class CryptographyServiceImpl implements CryptographyService {
     @Override
@@ -39,7 +41,7 @@ public class CryptographyServiceImpl implements CryptographyService {
     @Override
     public void encodeStringUsingMIMETypeBase64(String originalInput) {
 
-        String encodedString = Base64.getMimeEncoder().encodeToString(originalInput.getBytes());
+        String encodedString = Base64.getMimeEncoder().encodeToString( originalInput.getBytes() );
 
         System.out.println( "encodedString MIME = " + encodedString );
     }
@@ -47,9 +49,51 @@ public class CryptographyServiceImpl implements CryptographyService {
     @Override
     public void decodeStringUsingMIMETypeBase64(String encodedString) {
 
-        byte[] decodedBytes = Base64.getMimeDecoder().decode(encodedString);
-        String decodedString = new String(decodedBytes);
+        byte[] decodedBytes = Base64.getMimeDecoder().decode( encodedString );
+        String decodedString = new String( decodedBytes );
 
         System.out.println( "decodedString MIME = " + decodedString );
+    }
+
+    @Override
+    public void ASCIIToHexConverter(String originalInput) {
+
+        StringBuilder forLoopHex = new StringBuilder();
+
+        for (char c : originalInput.toCharArray()) {
+            forLoopHex.append( Integer.toHexString( c ) );
+        }
+
+        System.out.println( "forLoopHex = " + forLoopHex );
+
+        // Using Stream...
+
+        String streamHex = originalInput.chars()
+                .mapToObj( Integer::toHexString )
+                .collect( Collectors.joining() );
+
+        System.out.println( "streamHex = " + streamHex );
+    }
+
+    @Override
+    public void HexToASCIIConverter(String asciiInputValues) {
+
+        StringBuilder forLoopASCII = new StringBuilder();
+
+        for (int i = 0; i < asciiInputValues.length(); i += 2) {
+
+            String str = asciiInputValues.substring( i, i + 1 );
+            forLoopASCII.append( (char) Integer.parseInt( str, 16 ) );
+        }
+
+        System.out.println( "forLoopASCII = " + forLoopASCII );
+
+        // Using Stream...
+
+        String streamASCII = asciiInputValues.chars()
+                .mapToObj( c -> String .valueOf( (char) Integer.parseInt( "" + (char) c + (char) c, 16)) )
+                .collect(Collectors.joining());
+
+        System.out.println( "streamASCII = " + streamASCII );
     }
 }
