623798d6a8d62f123d25b7dd1c7474f0162883b8
GouthamGuna <ggowthamsankar@gmail.com>
2024-03-29 04:37:34 +0530

    Update EasyProblemsTest.java

--- experimental-lab-2024/src/test/java/in/dev/gmsk/leetcode/EasyProblemsTest.java
+++ experimental-lab-2024/src/test/java/in/dev/gmsk/leetcode/EasyProblemsTest.java
@@ -1,4 +1,24 @@
+package in.dev.gmsk.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static in.dev.gmsk.leetcode.EasyProblems.*;
 import static org.junit.jupiter.api.Assertions.*;
+
 class EasyProblemsTest {
-  
+
+    @Test
+    void testMergeStringsAlternately() {
+
+        String W_One = "abc";
+        String W_Two = "gmsk";
+
+        String actual = MergeStringsAlternately(W_One, W_Two);
+        String actualOne =MergeStringsAlternatelyRefactor(W_One, W_Two);
+
+        String expected = "agbmcsk";
+
+        assertEquals(expected, actual);
+        assertEquals(expected, actualOne);
+    }
 }
