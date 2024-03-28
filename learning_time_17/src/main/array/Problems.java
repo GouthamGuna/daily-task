4d1871cd4489c35daab1b073cb4ced883dff6981
Gowtham Sankar Gunasekaran <88366848+GouthamGuna@users.noreply.github.com>
2024-03-29 04:44:41 +0530

    Update Problems.java

--- learning_time_17/src/main/array/Problems.java
+++ learning_time_17/src/main/array/Problems.java
@@ -1,2 +1,26 @@
-package main.array;public class Problems {
+package main.array;
+
+public class Problems {
+
+  public static void main(String[] args) {
+        int[] nums = {1,1,2};
+
+        Arrays.stream(removeDuplicatesFromSortedArray(nums)).forEach(System.out::println);
+
+    }
+
+    public static int[] removeDuplicatesFromSortedArray(int[] ar) {
+
+        int j = 1;
+        int[] returnArr = new int[ar.length];
+
+        for (int i = 1; i < ar.length; i++) {
+            if (ar[i] != ar[i - 1]) {
+                ar[j] = ar[i];
+                j++;
+                returnArr[i] = ar[j];
+            }
+        }
+        return returnArr;
+    }
 }
