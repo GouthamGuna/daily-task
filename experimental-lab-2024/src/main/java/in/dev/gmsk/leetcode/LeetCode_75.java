ff28b6e0b6a137e0ddc4192f083554122964c245
GouthamGuna <ggowthamsankar@gmail.com>
2024-04-22 19:47:51 +0530

    Update LeetCode_75.java

--- experimental-lab-2024/src/main/java/in/dev/gmsk/leetcode/LeetCode_75.java
+++ experimental-lab-2024/src/main/java/in/dev/gmsk/leetcode/LeetCode_75.java
@@ -7,19 +7,19 @@ public class LeetCode_75 {
 
     public static String task_4_ReverseVowels(String s) {
 
-        if (s==null || s.isEmpty()) return s;
+        if (s == null || s.isEmpty()) return s;
 
         int i = 0;
-        int j = s.length()-1;
+        int j = s.length() - 1;
 
         char[] chars = s.toCharArray();
 
-        while(i<j){
-            while(i<j && !vowels.get().contains(chars[i]+"")){
+        while (i < j) {
+            while (i < j && !vowels.get().contains(chars[i] + "")) {
                 i++;
             }
 
-            while(i<j && !vowels.get().contains(chars[j]+"")){
+            while (i < j && !vowels.get().contains(chars[j] + "")) {
                 j--;
             }
 
@@ -40,13 +40,13 @@ public class LeetCode_75 {
 
     public static final Function<int[], Boolean> increasingTripletSubsequence = (num) -> {
 
-        if (num == null || num.length < 3){
+        if (num == null || num.length < 3) {
             return false;
         }
 
         int i = Integer.MAX_VALUE, j = Integer.MIN_VALUE;
 
-        for(int n: num) {
+        for (int n : num) {
             if (n <= i) {
                 i = n;
             } else if (n <= j) {
@@ -56,6 +56,29 @@ public class LeetCode_75 {
             }
         }
 
-         return false;
+        return false;
+    };
+
+    public static Function<char[], Integer> stringCompression = chars -> {
+
+        int c = 1;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < chars.length; i++) {
+
+            if (i + 1 < chars.length && chars[i] == chars[i + 1]) {
+                c++;
+            } else {
+                sb.append(chars[i]);
+                if (c > 1) {
+                    sb.append(c);
+                }
+                c = 1;
+            }
+        }
+        System.out.print(sb);
+        char[] compressedChars = sb.toString().toCharArray();
+        System.arraycopy(compressedChars, 0, chars, 0, compressedChars.length);
+        return sb.length();
     };
 }
