e8b149e66870e4b6263d1ca3e8a3804b38a4427f
GouthamGuna <ggowthamsankar@gmail.com>
2024-05-28 18:10:44 +0530

    Update Solutions.java

--- experimental-lab-2024/src/main/java/in/dev/gmsk/hackerrank/Solutions.java
+++ experimental-lab-2024/src/main/java/in/dev/gmsk/hackerrank/Solutions.java
@@ -147,4 +147,22 @@ public class Solutions {
 
         return Math.max(dp[N - 1][0], dp[N - 1][1]);
     }
+
+    public static int hourglassSum(List<List<Integer>> arr) {
+        if (arr == null || arr.size() < 3 || arr.get(0).size() < 3) {
+            throw new IllegalArgumentException("The input list must be at least 3x3 in size.");
+        }
+
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i <= arr.size() - 3; i++) {
+            for (int j = 0; j <= arr.get(i).size() - 3; j++) {
+                int sum = arr.get(i).get(j) + arr.get(i).get(j + 1) + arr.get(i).get(j + 2)
+                        + arr.get(i + 1).get(j + 1)
+                        + arr.get(i + 2).get(j) + arr.get(i + 2).get(j + 1) + arr.get(i + 2).get(j + 2);
+                maxSum = Math.max(maxSum, sum);
+            }
+        }
+        return maxSum;
+    }
+
 }
