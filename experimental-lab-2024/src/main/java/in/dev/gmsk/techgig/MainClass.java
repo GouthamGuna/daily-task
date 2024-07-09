a122b5d2732f5b530325b17ba041c753ab15f90f
GouthamGuna <ggowthamsankar@gmail.com>
2024-07-09 10:58:13 +0530

    Update MainClass.java

--- experimental-lab-2024/src/main/java/in/dev/gmsk/techgig/MainClass.java
+++ experimental-lab-2024/src/main/java/in/dev/gmsk/techgig/MainClass.java
@@ -5,6 +5,13 @@ public class MainClass {
     public static void main(String[] args) {
         int num = 1234;
         System.out.println("reverseNumber = " + reverseNumber(num));
+
+        int[] arr = {10, 22, 55, 20, 1, 4, 5, 2, 3, 12};
+        bubbleSort(arr);
+        System.out.println("Sorted array: ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
     }
 
     private static int reverseNumber(int num) {
@@ -17,5 +24,22 @@ public class MainClass {
             num = num / 10;
         }
         return reverse;
+    }
+
+    // Time Complexity : O(n^2)
+    private static void bubbleSort(int[] numbers) {
+
+        int n = numbers.length;
+        int temp = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
     }
 }
