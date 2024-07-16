b7d44b8de2bd95bee27c65a09b2dfece0a44762a
GouthamGuna <ggowthamsankar@gmail.com>
2024-07-16 07:34:03 +0530

    initial commit

---
+++ golang_hr_challenges/src/algorithms/fibonacci.go
@@ -0,0 +1,16 @@
+package algorithms
+
+import "fmt"
+
+func fib(x int64) int64 {
+	if x <= 1 {
+		return x
+	}
+	return fib(x-1) + fib(x-2)
+}
+
+func fibonacci(x int64, y int64) {
+	for i := x; i < y; i++ {
+		fmt.Printf("Fibonacci no :renage : %d : %d ", i, fib(i))
+	}
+}
