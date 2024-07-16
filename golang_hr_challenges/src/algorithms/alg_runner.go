d1e8d5a704c00f62e30ceaef658361c01dc24720
Goutham <ggowthamsankar@gmail.com>
2024-07-16 11:39:06 +0530

    Update alg pkg

--- golang_hr_challenges/src/algorithms/alg_runner.go
+++ golang_hr_challenges/src/algorithms/alg_runner.go
@@ -5,13 +5,17 @@ import "fmt"
 func ALG_Runners() {
 	fmt.Printf("\nRunning Algorithms ALG_Runners\n")
 
+	//Bubbole Sorting
 	arr := []int64{10, 22, 55, 20, 1, 4, 5, 2, 3, 12}
 	bubbleSort(arr)
 	fmt.Println("Sorted Array: ", arr)
 
+	// Fibonacci
 	fibonacci(1, 10)
 
+	// Divisiable Sum Pair
 	arr1 := []int32{1, 3, 2, 6, 1, 2}
+	var arrPointer *[]int32 = &arr1
 
-	fmt.Printf("Divisible Sum pairs  : %d\n", divisibleSumPairs(int32(6), int32(3), arr1))
+	fmt.Printf("Divisible Sum pairs  : %d\n", divisibleSumPairs(int32(6), int32(3), arrPointer))
 }
--- golang_hr_challenges/src/algorithms/divisible_sum_pairs.go
+++ golang_hr_challenges/src/algorithms/divisible_sum_pairs.go
@@ -1,12 +1,11 @@
 package algorithms
 
-func divisiableBySum(arr []int, k int, l int) int {
+func divisibleSumPairs(n int32, k int32, ar *[]int32) int32 {
+	var count int32 = 0
 
-	var count = 0
-
-	for i := 0; i < l; i++ {
-		for j := i + 1; j < l; j++ {
-			if (arr[i] + arr[j]) % k == 0 {
+	for i := 0; i < int(n); i++ {
+		for j := i + 1; j < int(n); j++ {
+			if ((*ar)[i] + (*ar)[j]) % k == 0 {
 				count++
 			}
 		}
